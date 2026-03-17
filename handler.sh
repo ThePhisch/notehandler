841408e686d0689746b82947aab3322c18f9b8b0
Anton Schwarz <anton.schwarz@cern.ch>
2026-03-17 18:25:07 +0100

    Push and pull work

--- handler.sh
+++ handler.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PULL_INTERVAL=1  # seconds
+PULL_INTERVAL=10  # seconds
 NOTE_FOLDER=./test  # should be global folder
 
-echo "Running handler.sh with NOTE_FOLDER=${NOTE_FOLDER}"
+echo "Running handler.sh with NOTE_FOLDER=${NOTE_FOLDER}."
 
 if [[ -z "$(command -v entr)" ]]
 then
@@ -18,21 +18,59 @@ then
 fi
 
 exit_peacefully() {
+	# listen to Ctrl-C, propagate this to the background loop looking for
+	# data to pull from remote by killing the process identified by PID.
 	echo "Exiting peacefully."
+	kill "${PULL_LOOP_PID}"
 	exit 0
 }
 trap exit_peacefully SIGINT
 
-react_to_change() {
-	echo "Some files have changed in ${NOTE_FOLDER}"
+react_to_local_change() {
+	# On being called, check if there is a change relevant to git. Commit
+	# and push.
+	echo "$(date): Some files have changed in ${NOTE_FOLDER}."
+	cd "${NOTE_FOLDER}" || return
 	git add .
-	git commit -m "Automatic commit by handler.sh on ${NOTE_FOLDER} at ${date}."
-	git push origin main
+	if ! git diff --staged --quiet
+	then
+		git commit -m "Automatic commit in ${NOTE_FOLDER} at $(date)"
+		git push origin main
+		echo "Back to main loop."
+	else
+		echo "False alarm."
+	fi
 }
 export NOTE_FOLDER
-export -f react_to_change
+export -f react_to_local_change
 
-while sleep 0.5
+pull_remote_change() {
+	# this function is run in the current shell, not a subshell
+	# hence we use `return` and `cd -` because we want to reset the global
+	# state
+	cd "${NOTE_FOLDER}" || return
+	echo "$(date): checking for remote changes."	
+	git fetch origin main >/dev/null 2>&1
+	git rebase origin/main
+	cd - >/dev/null || return
+}
+export -f pull_remote_change
+
+while true
 do
-	find ${NOTE_FOLDER} -type f | entr -dnr bash -c 'react_to_change'
+	pull_remote_change
+	sleep "${PULL_INTERVAL}"
+done &
+# save the PID of this background loop so we can kill it later.
+PULL_LOOP_PID=$!
+
+# NOTE using `sleep` here would mean that a new entr subprocess is spawned regularly
+# We actually want a new subprocess to start immediately, but only once the
+# current one finishes!
+# Also ignore git internals.
+while true
+do
+	find ${NOTE_FOLDER} -type f \
+		! -path "${NOTE_FOLDER}/.git/*" \
+		| entr -dnr bash -c 'react_to_local_change'
 done
